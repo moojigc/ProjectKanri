c70048d9267028576db751549c724593fb1fdbf0
STadds <staddi@gmail.com>
2020-07-02 17:57:41 -0400

    utils folder created, started actions

---
+++ client/src/utils/actions.js
@@ -0,0 +1,10 @@
+// PROJECT ACTIONS
+export const ADD_PROJECT = "ADD_PROJECT";
+export const UPDATE_PROJECT = "UPDATE_PROJECT";
+
+// TASK ACTIONS
+export const ADD_TASK = "ADD_TASK";
+export const UPDATE_TASK = "UPDATE_TASK";
+
+// USER ACTIONS
+export const UPDATE_USER = "UPDATE_USER";
