e2bd4d0a9a9bd7da7e5f6be003bb0785ca9a8725
mpiarahman16 <mpiarahman@gmail.com>
2020-07-05 18:50:09 -0400

    adding wrapper

--- client/src/pages/Task/index.js
+++ client/src/pages/Task/index.js
@@ -20,9 +20,21 @@ export default function Task() {
      <Container maxWidth= "lg" component ="main">
        <h1 className= "heading">Task: Title</h1>
        <Wrapper>
-         <Grid container justify= "center" spscing={2}>
+         <Grid container justify= "center" spacing={2}>
           <Grid item sm= {12}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Grid></Grid>
        </Wrapper>
+      <Wrapper style= {{marginTop: "1rem"}}><Grid item sm= {12}>Text 1</Grid>
+      </Wrapper>
+      <Wrapper style= {{marginTop: "1rem"}}><Grid item sm= {12}>Text 1</Grid>
+      </Wrapper>
+      <Wrapper style= {{marginTop: "1rem"}}><Grid item sm= {12}>Text 1</Grid>
+      </Wrapper>
+      <Wrapper style= {{marginTop: "1rem"}}><Grid item sm= {12}>Text 1</Grid>
+      </Wrapper>
+      <Wrapper style= {{marginTop: "1rem"}}><Grid item sm= {12}>Text 1</Grid>
+      </Wrapper>
+      <Wrapper style= {{marginTop: "1rem"}}><Grid item sm= {12}>Text 1</Grid>
+      </Wrapper>
      </Container>
   )
 };
