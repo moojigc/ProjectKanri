ef40b0a6809024d355725b671a586fed445a960a
Moojig Battsogt <moojigc@gmail.com>
2020-07-06 18:44:46 -0400

    started task router with fake data

---
+++ server/routes/task-router.js
@@ -0,0 +1,24 @@
+const { User, Task } = require("../models/");
+const passport = require("../config/passport");
+
+/**
+ * Handles user login, status, registration, etc.
+ * @param {import("express").Router} router
+ */
+module.exports = (router) => {
+	router.get("/api/task/:id", async (req, res) => {
+		try {
+			let fakeTask = {
+				_id: "al2r3vq32",
+				title: "Test Task",
+				description: "hello world",
+				assignedTo: "Moojig"
+			};
+			let task = await Task.findById(req.params.id);
+			res.json(task).end();
+		} catch (error) {
+			console.error(error);
+			res.json("error").end();
+		}
+	});
+};
