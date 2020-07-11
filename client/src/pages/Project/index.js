c32f60fabca1f5d483d7f522e80fd86da6828d57
STadds <staddi@gmail.com>
2020-07-11 13:38:52 -0400

    add Project route to App.js

---
+++ client/src/pages/Project/index.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState, useContext } from "react";
+import { makeStyles } from "@material-ui/core";
+// import { Wrapper } from "../../components/MiniComponents";
+import { Title, Wrapper, ButtonLink } from "../../components/MiniComponents";
+import Container from "@material-ui/core/Container";
+import { Grid, Card, CardContent, Typography, CardHeader } from "@material-ui/core/";
+
+const Project = () => {
+
+}
+
+export default Project;
