3efd5b3f05f3f5416442e92e70031b5e8e0b5f00
STadds <staddi@gmail.com>
2020-07-14 22:58:45 -0400

    add fab create project to dashboard

--- client/src/components/CreateDialog/index.js
+++ client/src/components/CreateDialog/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Box, TextField, Button, Typography, Grid, InputAdornment } from "@material-ui/core";
 import {
@@ -9,46 +9,63 @@ import {
 	DialogTitle
 } from "@material-ui/core";
 import { UserContext } from "../../utils/UserContext";
+import clsx from "clsx";
+import { set } from "mongoose";
+
+const useStyles = makeStyles(theme => ({
+    formfield: {
+        marginBottom: "1rem"
+    }
+}))
 
 const CreateDialog = ({ open, setOpen }) => {
+
+    const classes = useStyles();
+    
 	const [projectForm, setProjectForm] = useState({});
 	const { setUser } = useContext(UserContext);
 
 	const handleInputChange = (event) => {
 		const { name, value } = event.target;
-		setProjectForm({ ...projectForm, [name]: value });
+        setProjectForm({ ...projectForm, [name]: value });
     };
     
     const handleSubmit = () => {
         console.log("submitting: ", projectForm);
+        setProjectForm({});
+        setOpen(false);
     }
 
 	return (
-		<Dialog open={open} onClose={setOpen(false)} aria-labelledby="create-dialog-title">
-			<DialogTitle id="create-dialog-title">Create New Project</DialogTitle>
-			<DialogContent>
+		<Dialog open={open} onClose={() => setOpen(false)} aria-labelledby="create-dialog-title">
+			<DialogTitle id="create-dialog-title" onClose={() => setOpen(false)}>Create New Project</DialogTitle>
+			<DialogContent dividers>
 				<DialogContentText>
 					Please enter the required information to create a new project.
 				</DialogContentText>
 				<form>
 					<TextField
-						required
+                        required
+                        size="small"
+                        className= {clsx(classes.formfield)}
 						label="Project Title"
 						name="title"
 						onChange={handleInputChange}
 						variant="outlined"></TextField>
 					<TextField
-						required
-						fullWidth
+                        required
+                        multiline
+                        fullWidth
+                        size="small"
 						label="Project Description"
-						rows={4}
+						rows={6}
 						name="description"
 						onChange={handleInputChange}
 						variant="outlined"></TextField>
 				</form>
 			</DialogContent>
 			<DialogActions>
-				<Button onClick={setOpen(false)} color="secondary">
+				<Button onClick={() => setOpen(false)} color="secondary">
 					Cancel
 				</Button>
 				<Button onClick={handleSubmit} variant="contained" color="primary">
