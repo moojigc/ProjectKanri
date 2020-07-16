413191af8a0d8498c3a6542b2ec510b2ada96147
Moojig Battsogt <moojigc@gmail.com>
2020-07-16 19:21:20 -0400

    updated task get route

--- server/routes/task-router.js
+++ server/routes/task-router.js
@@ -1,5 +1,19 @@
 const { User, Task } = require("../models/");
 const passport = require("../config/passport");
+/**
+ *
+ * @param {string} message
+ * @param {"error" | "success"} type
+ */
+const flash = (message, type) => {
+	return {
+		flash: {
+			message: message,
+			type: type
+		}
+	};
+};
+const serverError = (res) => res.status(500).json(flash("Internal server error.", "error")).end();
 
 /**
  * Handles user login, status, registration, etc.
@@ -7,19 +21,13 @@ const passport = require("../config/passport");
  */
 module.exports = (router) => {
 	router.get("/api/task/:id", async (req, res) => {
-		console.log("IN ROUTE: /api/task/:id" + req.params.id);
+		// console.log("IN ROUTE: /api/task/:id" + req.params.id);
 		try {
-			let fakeTask = {
-				_id: "al2r3vq32",
-				title: "Test Task",
-				description: "hello world",
-				assignedTo: "Moojig"
-			};
 			let task = await Task.findById(req.params.id);
 			res.json(task).end();
 		} catch (error) {
 			console.error(error);
-			res.json("error").end();
+			serverError(res);
 		}
 	});
 };
