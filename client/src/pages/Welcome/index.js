9d028d92355977c93d635bff062425f6e4a063b3
Moojig Battsogt <moojigc@gmail.com>
2020-07-21 02:53:13 -0400

    Added registration fields to welcome page

--- client/src/pages/Welcome/index.js
+++ client/src/pages/Welcome/index.js
@@ -7,7 +7,7 @@ import { UserContext } from "../../utils/UserContext";
 import { useHistory } from "react-router-dom";
 import { makeStyles, Box, Typography, useMediaQuery, Paper } from "@material-ui/core";
 import { ButtonLink, SakuraBranches } from "../../components/MiniComponents";
-import { ArrowForward } from "@material-ui/icons";
+import { ArrowForward, Send } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
 	welcome: {
@@ -20,8 +20,27 @@ const useStyles = makeStyles((theme) => ({
 
 const Welcome = () => {
 	const [continueSignUp, setContinue] = useState(false);
+	const [valid, setValid] = useState(false);
 	const email = useRef(null);
 	const isMobile = useMediaQuery("(max-width: 997px)");
+	const [signUpDetails, setSignUpDetails] = useState({
+		username: "",
+		password: "",
+		password2: "",
+		firstName: "",
+		lastName: "",
+		email: ""
+	});
+	const handleChangeUserDetails = ({ target }) => {
+		let { name, value } = target;
+		setSignUpDetails({
+			...signUpDetails,
+			[name]: value
+		});
+		if (Object.values(signUpDetails).filter((s) => s !== "" || s === null).length === 0) {
+			setValid(true);
+		}
+	};
 	const { user, setUser } = useContext(UserContext);
 	const classes = useStyles();
 	const history = useHistory();
@@ -33,9 +52,26 @@ const Welcome = () => {
 			email: email.current.value
 		});
 	};
+	const Input = ({ defaultValue, name, label, type, margin }) => {
+		return (
+			<TextField
+				defaultValue={defaultValue}
+				color="secondary"
+				fullWidth
+				name={name}
+				margin={margin}
+				label={label}
+				variant="filled"
+				required
+				aria-required
+				type={type || "text"}
+				onChange={handleChangeUserDetails}
+			/>
+		);
+	};
 	return (
 		<Container maxWidth="xl" disableGutters className={classes.welcome}>
-			<SakuraBranches zIndex={1} opacity="0.5" width={isMobile? "15rem" : "25rem"} />
+			<SakuraBranches zIndex={1} opacity="0.5" width={isMobile ? "15rem" : "25rem"} />
 			<Box className="welcome" zIndex={2} position="relative">
 				<Grid container>
 					<Typography variant={isMobile ? "h2" : "h1"} component="h1" color="textSecondary">
@@ -49,36 +85,64 @@ const Welcome = () => {
 				</Grid>
 			</Box>
 			<form onSubmit={handleSubmit} className="welcome-form">
-				<Grid container>
-					<TextField
-						inputRef={email}
-						type="email"
-						margin="normal"
-						variant="filled"
-						fullWidth
-						color="secondary"
-						label="Enter your email address to get started."
-					/>
-				</Grid>
 				{continueSignUp ? (
-					<Grid container>
-						<TextField 
-							required
-							aria-required
-							label="Username"
-							name="username"
-							fullWidth
-						/>
-					</Grid>
+					<Box>
+						<Grid container spacing={2}>
+							<Grid item xs={12} lg={6}>
+								<Input default={signUpDetails.email} label="Email" name="email" type="email" />
+							</Grid>
+							<Grid item xs={12} lg={6}>
+								<Input label="Username" name="username" />
+							</Grid>
+						</Grid>
+						<Grid container spacing={2}>
+							<Grid item xs={12} lg={6}>
+								<Input label="Password" name="password" type="password" />
+							</Grid>
+							<Grid item xs={12} lg={6}>
+								<Input label="Confirm password" name="password2" type="password" />
+							</Grid>
+						</Grid>
+						<Grid container spacing={2}>
+							<Grid item xs={12} lg={6}>
+								<Input label="First Name" name="firstName" />
+							</Grid>
+							<Grid item xs={12} lg={6}>
+								<Input label="Last Name" name="lastName" />
+							</Grid>
+						</Grid>
+						{valid && (
+							<Button variant="contained" endIcon={<Send />}>
+								Sign up
+							</Button>
+						)}
+					</Box>
 				) : (
-					<Grid container style={{zIndex: 3}}>
-						<Button style={{ marginRight: "1rem" }} onClick={() => setContinue(true)} variant="contained" color="primary" endIcon={<ArrowForward />}>
-							Continue sign up
-						</Button>
-						<ButtonLink style={{ marginTop: isMobile ? "0.5rem" : "initial" }} variant="contained" color="secondary" to="/login" type="button">
-							Login with exisiting account
-						</ButtonLink>
-					</Grid>
+					<React.Fragment>
+						<Grid container>
+							<Input margin="normal" type="email" label="Enter your email address to get started." />
+						</Grid>
+						<Grid container style={{ zIndex: 3 }}>
+							<Button
+								style={{ marginRight: "1rem" }}
+								onClick={() => setContinue(true)}
+								variant="contained"
+								color="primary"
+								endIcon={<ArrowForward />}
+							>
+								Continue sign up
+							</Button>
+							<ButtonLink
+								style={{ marginTop: isMobile ? "0.5rem" : "initial" }}
+								variant="contained"
+								color="secondary"
+								to="/login"
+								type="button"
+							>
+								Login with exisiting account
+							</ButtonLink>
+						</Grid>
+					</React.Fragment>
 				)}
 			</form>
 		</Container>
