8da8c1ea5f049e43ecb461d715158a7d18cc8453
Moojig Chimid <me@moojig.dev>
2022-12-14 12:48:31 -0500

    .js -> .jsx update

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,14 @@ import reactRefresh from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
 import { VitePWA } from "vite-plugin-pwa";
 
+const cacheDir = process.env.NODE_ENV === "development-docker" ? "/app/node_modules/.vite" : "node_modules/.vite";
+
 export default defineConfig({
 	server: {
+		host: true,
 		port: process.env.PORT || 5050
 	},
+	cacheDir,
 	build: {
 		outDir: "build"
 	},
